cb543c6c8c4bdbd8bbec6964f6836429befcc470
Adrian R. <roblesadrian03@gmail.com>
2025-01-21 13:41:02 +0100

    Botón consultar Formulario ListView (sin funcionalidad)

--- ConexBbDD/FormLView.Designer.vb
+++ ConexBbDD/FormLView.Designer.vb
@@ -25,15 +25,16 @@ Partial Class FormLView
         Me.ListView1 = New System.Windows.Forms.ListView()
         Me.nombre = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
         Me.edad = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.Button1 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'ListView1
         '
         Me.ListView1.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.nombre, Me.edad})
         Me.ListView1.HideSelection = False
-        Me.ListView1.Location = New System.Drawing.Point(57, 39)
+        Me.ListView1.Location = New System.Drawing.Point(189, 37)
         Me.ListView1.Name = "ListView1"
-        Me.ListView1.Size = New System.Drawing.Size(678, 377)
+        Me.ListView1.Size = New System.Drawing.Size(590, 372)
         Me.ListView1.TabIndex = 0
         Me.ListView1.UseCompatibleStateImageBehavior = False
         Me.ListView1.View = System.Windows.Forms.View.Details
@@ -46,11 +47,21 @@ Partial Class FormLView
         '
         Me.edad.Text = "Edad"
         '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(22, 37)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(147, 32)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Consultar"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
         'FormLView
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.ListView1)
         Me.Name = "FormLView"
         Me.Text = "FormLView"
@@ -61,4 +72,5 @@ Partial Class FormLView
     Friend WithEvents ListView1 As ListView
     Friend WithEvents nombre As ColumnHeader
     Friend WithEvents edad As ColumnHeader
+    Friend WithEvents Button1 As Button
 End Class
